3f95fcbab636cbce5e467601aeee62676b40286e
nicolasdhnr <njd20@ic.ac.uk>
2022-12-25 00:20:47 +0100

    🔧fix: Fix attempt to firebase gmail login

--- frontend/koalingo/src/firebase.js
+++ frontend/koalingo/src/firebase.js
@@ -20,14 +20,15 @@ import {
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
-};
+    apiKey: "AIzaSyAqk2JRGZyX4SnnanRYLYlN8839G_9w2go",
+    authDomain: "koalingo-dc436.firebaseapp.com",
+    databaseURL: "https://koalingo-dc436-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "koalingo-dc436",
+    storageBucket: "koalingo-dc436.appspot.com",
+    messagingSenderId: "314932299156",
+    appId: "1:314932299156:web:70fd245e72e91e6983fb81",
+    measurementId: "G-Y7ZP85BTB0"
+  };
 
 
 const app = initializeApp(firebaseConfig);
