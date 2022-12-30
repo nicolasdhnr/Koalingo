d2dc64586dfbee9f8bd01d8578f989e248aee66f
nicolasdhnr <njd20@ic.ac.uk>
2022-12-30 02:12:25 +0100

    :rocket: Added simple login page

--- frontend/koalingo/src/App.js
+++ frontend/koalingo/src/App.js
@@ -1,10 +1,11 @@
 import {
   Routes,
   Route,
+  useNavigate,
   useNavigationType,
   useLocation,
 } from "react-router-dom";
-
+import { getAuth } from "firebase/auth";
 import Home from "./pages/Home";
 import PlayerMemorizing from "./pages/PlayerMemorizing";
 import SetTimer from "./pages/SetTimer";
@@ -15,31 +16,71 @@ import PlayerWelcome from "./pages/PlayerWelcome";
 import HostProgressTracker from "./pages/HostProgressTracker";
 import HostLobby from "./pages/HostLobby";
 import Login from "./pages/Login";
-
+import Register from "./pages/Register";
 
 import Web19201 from "./pages/Web19201";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import { useCallback, createContext } from "react";
 
+import {logout} from "./firebase";
+
+
+const AuthContext = createContext({
+  auth: null,
+  setAuth: () => {},
+  user: null,
+});
 
 
 function App() {
   const action = useNavigationType();
   const location = useLocation();
   const pathname = location.pathname;
+  const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (action !== "POP") {
       window.scrollTo(0, 0);
     }
+    if (!user){
+      navigate("/")
+    }
   }, [action]);
+
+  // Logout Button conditionally renders if user is not logged in
+  
+
+  const onLogoutButtonClick = useCallback(async () => {
+    useContext(AuthContext);
+    await signOut(auth);
+    console.log(user)
+  }, [auth]);
+
+
+
+
+  
 
   //TODO: ADD Meta Tags
 
   return (
+    <div >
+    <h1> Koalingo </h1>
+    {user &&  <button onClick={logout}>Logout</button>}
+    
+    <AuthContext.Provider value={{auth, user }}>
     <Routes>
+     
+
       <Route path="/" element={<Login />} />
 
       <Route path="/player/welcome" element={<PlayerWelcome />} />
+
+      <Route path="/register" element={<Register />} />
 
       <Route path="/host/set/timer" element={<SetTimer />} />
 
@@ -60,7 +101,11 @@ function App() {
       <Route path="/home" element={<Home />} />
 
       <Route path="/web-1920-1" element={<Web19201 />} />
+
+      <Route path="*" element={<h1> Page not Found </h1>} />
     </Routes>
+    </AuthContext.Provider>
+    </div>
   );
 }
 export default App;
