5570c54145c5c96583e8c7de23962c0bdf42386a
gurooria <gloriasun528@gmail.com>
2023-01-06 00:12:11 +0000

    ✨frontend: email login & register pages styled with screen responsiveness
    Email login & registration pages now properly styled, respond to screen size changes

--- frontend/koalingo/src/components/button/Button.js
+++ frontend/koalingo/src/components/button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import './button.css';
 import checkScreenSize from "../rectangleWrapper/CheckScreenSize";
 
-const STYLES = ['white', 'purple', 'gold','red','bgColor']
+const STYLES = ['white', 'purple', 'gold','red','bgColor','transparent']
 
 const Button = ({
     btnText,
--- frontend/koalingo/src/pages/home/Register.js
+++ frontend/koalingo/src/pages/home/Register.js
@@ -1,11 +1,15 @@
 import { useCallback, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./register.module.css";
+import stylesLogin from "./login.module.css";
+import stylesEmailLogin from "./emailLogin.module.css"
+import stylesRegister from "./register.module.css"
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { registerWithEmailAndPassword } from "../../firebase";
 import { AuthContext } from "../../App";
+import Button from "../../components/button/Button";
+import RecWrapper from "../../components/rectangleWrapper/Wrapper";
 
 const Register = () => {
 
@@ -18,6 +22,10 @@ const Register = () => {
         confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
     });
 
+    const onGoBackClick = useCallback(() => {
+        navigate("/");
+     }, [navigate]);
+
     useEffect(() => {
         if (user) {
             navigate("/home");
@@ -29,8 +37,8 @@ const Register = () => {
     });
     // Navigate to home page on submit
 
-    const onLoginButtonClick = useCallback(() => {
-       navigate("/");
+    const onKoaUserButtonClick = useCallback(() => {
+       navigate("/login");
     }, [navigate]);
 
     const onSubmitClick = useCallback((data) => {
@@ -38,61 +46,60 @@ const Register = () => {
     }, [navigate]);
 
     return (
-        <div className={styles.login1}>
-        <form onSubmit={handleSubmit(onSubmitClick)}>
-        
-        <div className={styles.login1Child} />
-        <div className={styles.login}>
-            <button
-            className={styles.loginChild}
-            onClick={onLoginButtonClick}
-            />
-            <div className={styles.login2}>Login</div>
-            <div className={styles.alreadyAKoalingoUser}>
-            Already a Koalingo user?
-            </div>
+        <div className={stylesLogin.loginPage}>
+            <img className={stylesEmailLogin.koalingoLogo} alt="" src="../koalingo_logo.svg" />
+
+            <form onSubmit={handleSubmit(onSubmitClick)}>
+                <div className={stylesRegister.loginWrapper}> 
+                    <RecWrapper size= "heightFit"
+                    children1={
+                        <div className={stylesRegister.registerText}> Registration </div>
+                    }
+                    children2={
+                        <input 
+                        className={stylesRegister.email} 
+                        name="email"
+                        placeholder="email"
+                        {...register("email")}
+                        />
+                    }
+                    children3={
+                        <input
+                        className={stylesRegister.password}
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        {...register("password")}
+                        />
+                    }
+
+                    children4={
+                        <input
+                        className={stylesRegister.confirmPassword}
+                        name="confirmPassword"
+                        type="password"
+                        placeholder="confirm password"
+                        {...register("confirmPassword")}
+                        />
+                    }
+
+                    children8={
+                        <input className={stylesRegister.submitBtn} type="submit"  />
+                    }
+                    />
+                    <div className={stylesRegister.goBackWrapper}>
+                        <div className={stylesRegister.backText}
+                            onClick={onKoaUserButtonClick}><u>Already a Koalingo User?</u></div>
+                        <Button btnText="Go back" onClick={onGoBackClick}
+                                btnStyle="bgColor" length="btnLong" />
+                    </div>
+                    <p>{errors.email?.message}</p>
+                    <p>{errors.password?.message}</p>
+                    <p>{errors.confirmPassword?.message}</p>
+                </div>
+                
+            </form>
         </div>
-        <div className={styles.login1Item} />
-        <img
-            className={styles.allergiesPlanDeTravail11}
-            alt=""
-            src="../../koalingo_logo.svg"
-        />
-        <div className={styles.groupe61}>
-        
-            <input 
-            className={styles.email} 
-            name="email"
-            placeholder='email' 
-            {...register("email")}
-            />
-            <p>{errors.email?.message}</p>
-            <input
-            className={styles.password}
-            name="password"
-            type="password"
-            placeholder="Password"
-            {...register("password")}
-            />
-            <p>{errors.password?.message}</p>
-            <input
-            className={styles.confirm}
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirm password"
-            {...register("confirmPassword")}
-            />
-            <p>{errors.confirmPassword?.message}</p>
-            
-        </div>
-        <input className={styles.trac3} type="submit"  />
-        <input className={styles.submit} type="submit"  />
-        
-    
-        </form>
-        <b className={styles.registrationForm}>Registration form</b>
-        </div>
-       
     );
     };
 
