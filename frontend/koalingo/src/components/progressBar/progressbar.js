2498c3443f5b10c8ae171583fa5cc34b4bb1dbb5
mattdullapNW10 <98484425+mattdullapNW10@users.noreply.github.com>
2023-01-14 11:48:06 +0000

    Minor improvements on quizz

---
+++ frontend/koalingo/src/components/progressBar/progressbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+  
+const Progress_bar = ({bgcolor,progress,height}) => {
+     
+    const Parentdiv = {
+        height: height,
+        width: '300%',
+        backgroundColor: 'whitesmoke',
+        borderRadius: 40,
+        margin: 50
+      }
+      
+      const Childdiv = {
+        height: '100%',
+        width: `${progress}%`,
+        backgroundColor: bgcolor,
+       borderRadius:40,
+        textAlign: 'right'
+      }
+      
+      const progresstext = {
+        padding: 10,
+        color: 'black',
+        fontWeight: 900
+      }
+        
+    return (
+    <div style={Parentdiv}>
+      <div style={Childdiv}>
+        
+      </div>
+    </div>
+    )
+}
+  
+export default Progress_bar;
