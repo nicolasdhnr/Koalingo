55ab973511872a06c28de164837c40ea274f3a65
jl3720 <jameszcliu@gmail.com>
2023-01-14 18:45:22 +0000

    :hammer: Refactored onclick to more general buttons.

--- frontend/koalingo/src/pages/home/PlayerCustomise.js
+++ frontend/koalingo/src/pages/home/PlayerCustomise.js
@@ -1,93 +1,51 @@
-import { useCallback, useContext } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import stylesHome from "./home.module.css";
 import stylesLogin from "./login.module.css";
 import stylesCustomise from "./player-customise.module.css";
 import stylesEmailLogin from "./email-login.module.css";
 import { realtimedb } from "../../firebase";
-import { ref, onValue, update, set} from "firebase/database";
+import { ref, update} from "firebase/database";
 import React, { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { AuthContext } from "../../App";
 import RecWrapper from "../../components/rectangleWrapper/Wrapper";
-import Button from "../../components/button/Button";
 import "../../App"
-import {logout} from "../../firebase";
 
-// import {
-//   getFirestore,
-//   query,
-//   getDocs,
-//   collection,
-//   where,
-//   addDoc,
-//   setDoc,
-//   updateDoc,
-// } from "firebase/firestore";
-
-// Home
 export const PlayerCustomise = () => {
   const navigate = useNavigate();
-  const reference = ref(realtimedb, "games");
-  // const [gamePinEntered, setGamePinEntered] = useState("");
   const auth = getAuth();
   const user = auth.currentUser;
-  const {globNickname, setGlobNickname, character, setCharacter} = useContext(AuthContext);
+  const {setCharacter} = useContext(AuthContext);
   const [nickname, setNickname] = useState("");
 
   // Directly pulled from storage folder on firebase
   const girl_url = 'https://firebasestorage.googleapis.com/v0/b/koalingo-dc436.appspot.com/o/initial-wave-girl.gif?alt=media&token=f9a02670-c2fa-423b-877f-429c81f29f9d';
   const boy_url = 'https://firebasestorage.googleapis.com/v0/b/koalingo-dc436.appspot.com/o/initial-wave-boy.gif?alt=media&token=f87acbde-d8c5-4bad-8b8c-06564f39a0dc';
 
-  const testUserID = 'test';
   const {gamePin} = useContext(AuthContext)
 
   const addNickname = async () => {
     const reference = ref(realtimedb, "games/" + gamePin + "/players/" + user.uid);
     // Create a new game
-    await update(reference, {
-      "nickname" : nickname
-    }
-    );
+    await update(reference, {"nickname" : nickname});
   };
 
   const onChangeNickname = (event) => {
     setNickname(event.target.value);
   }
 
-  const onClickBoy = () => {
-    setCharacter('boy');
-    console.log(character);
+  const onClickChar = (event) => {
+    const char = event.currentTarget.id;
+    setCharacter(char);
+
     if (nickname != "") {
-      // setGlobNickname(nickname);
-      // console.log('Global Nickname: ', globNickname);
       console.log('Local nickname: ', nickname);
-      addNickname()
+      addNickname(event.currentTarget.id)
       navigate('/waiting');
     } else {
       alert('Sorry, you must choose a nickname!')
     }
-  }
-  const onClickGirl = () => {
-    setCharacter('girl');
-    console.log(character);
-    if (nickname != "") {
-      setGlobNickname(nickname);
-      console.log('Global Nickname: ', globNickname);
-      console.log('Local nickname: ', nickname);
-      navigate('/waiting');
-    } else {
-      alert('Sorry, you must choose a nickname!')
-    }
-  }
-
-  const onClickCheck = () => {
-    console.log(globNickname);
-    console.log(user);
-    console.log(user.uid);
-    console.log(gamePin);
-    addNickname(gamePin, user, globNickname);
-
   }
 
   return (
@@ -107,15 +65,14 @@ export const PlayerCustomise = () => {
                           <div className={stylesCustomise.characterWrapper}>
                           Choose your character!
                             <div className={stylesCustomise.characterWrapperChild}>
-                              <button className={stylesCustomise.child} onClick={onClickGirl}>
+                              <button id='girl' className={stylesCustomise.child} onClick={onClickChar}>
                                 <img src={girl_url} alt="girl failed to load"/>
                               </button>
-                              <button className={stylesCustomise.child} onClick={onClickBoy}>
+                              <button id='boy' className={stylesCustomise.child} onClick={onClickChar}>
                                 <img src={boy_url} alt="boy failed to load"/>
                               </button>
                             </div>
                           </div>
-                          <button onClick={onClickCheck}>Check context nickname</button>
                         </div>
                       }
           />
