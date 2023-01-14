26047621438ab55b643c991887c0ce56ecef1dcb
mattdullapNW10 <98484425+mattdullapNW10@users.noreply.github.com>
2023-01-14 03:13:18 +0000

    :rocket: Added quizz functionality for buttons

--- frontend/koalingo/src/pages/player/PlayerQuizz.js
+++ frontend/koalingo/src/pages/player/PlayerQuizz.js
@@ -1,6 +1,7 @@
 import styles from "./playerquizz.module.css";
 import { useCallback, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "../../components/button/Button";
 
 
 
@@ -11,8 +12,43 @@ const PlayerQuizz = () => {
     // Render state result component on click.  
   
   const onWordSelected = useCallback(() => {
+    console.log("hey");
     navigate("/player/result");
   }, [navigate]);
+
+  var sample_list = ["My", "name", "is", "matthieu","need","redbull"];
+  var good = "redbull";
+  const idx = sample_list.indexOf(good);
+  sample_list.pop(idx);
+
+  
+
+    // http://stackoverflow.com/questions/962802#962890
+    function shuffle(array) {
+      var tmp, current, top = array.length;
+      if(top) while(--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+      return array;
+  }
+
+  sample_list = shuffle(sample_list);
+  var a = (sample_list.slice(0,3));
+  a.push(good);
+  a = shuffle(a);
+  
+
+
+
+
+  const infos = {
+
+    current_words : a,
+  }
+  
 
 
 
@@ -25,16 +61,16 @@ const PlayerQuizz = () => {
       <div className={styles.hostquizwords1Inner} />
       <div className={styles.rectangleDiv} />
       <div className={styles.bedroomWrapper}>
-        <b className={styles.bedroom}>Bedroom</b>
+        <b className={styles.bedroom}>{infos.current_words[0]}</b>
       </div>
       <div className={styles.bedroomContainer}>
-        <b className={styles.bedroom}>Bedroom</b>
+        <b className={styles.bedroom}>{infos.current_words[1]}</b>
       </div>
       <div className={styles.bedroomFrame}>
-        <b className={styles.bedroom} onClick={onWordSelected}>Bedroom</b>
+        <b className={styles.bedroom} onClick={onWordSelected}>{infos.current_words[2]}</b>
       </div>
       <div className={styles.groupDiv}>
-        <button className={styles.bedroom}>Bedroom</button>
+        <button className={styles.bedroom}>{infos.current_words[3]}</button>
       </div>
       <img
         className={styles.allergiesPlanDeTravail11}
