06ecbfb01bae0e6a1ac38dc3509080e088d65403
jl3720 <jameszcliu@gmail.com>
2023-01-15 19:55:41 +0000

    :pencil: Documented home pages

--- koalingo/src/pages/home/Home.js
+++ koalingo/src/pages/home/Home.js
@@ -5,31 +5,39 @@ import stylesLogin from "./login.module.css";
 import stylesEmailLogin from "./emailLogin.module.css";
 import stylesLobby from "../host/hostLobby.module.css";
 import { realtimedb } from "../../firebase";
-import { ref, onValue, update, set} from "firebase/database";
+import { ref, onValue, update, set } from "firebase/database";
 import React, { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { AuthContext } from "../../App";
 import RecWrapper from "../../components/rectangleWrapper/Wrapper";
 import Button from "../../components/button/Button";
 import "../../App"
-import {logout} from "../../firebase";
+import { logout } from "../../firebase";
 
-// Home
+/**
+ * 
+ * @returns {JSX.Element} Home page element
+ */
 const Home = () => {
+  // Set navigation
   const navigate = useNavigate();
+
+  // Set database references
   const reference = ref(realtimedb, "games");
-  const [gamePinEntered, setGamePinEntered] = useState("");
   const auth = getAuth();
   const user = auth.currentUser;
-  const {gamePin, setGamePin} = useContext(AuthContext);
+
+  // Set states
+  const [gamePinEntered, setGamePinEntered] = useState("");
+  const { gamePin, setGamePin } = useContext(AuthContext);
   const [changePage, setChangePage] = useState(false);
 
   const handleTextChange = (event) => {
     setGamePinEntered(event.target.value);
   };
 
-  
-useEffect(() => {
+  // Set gamepin when Home component mounts
+  useEffect(() => {
     if (changePage) {
       console.log("changing page:" + gamePinEntered);
       console.log(typeof gamePinEntered)
@@ -40,6 +48,7 @@ useEffect(() => {
     }
   }, [changePage, gamePinEntered, setGamePin, navigate]);
 
+  // Define event listeners
 
   const checkGamePinOnSubmit = () => {
     console.log("Game pin entered:" + gamePinEntered);
@@ -52,11 +61,10 @@ useEffect(() => {
       if (data[gamePinEntered.toString()] !== undefined) {
         console.log("Game pin exists");
         setChangePage(true);
-        
+
       } else {
+        // Catch error if game pin does not exist
         alert("Game pin does not exist");
-        //TODO: Display error message
-        
       }
     }
     );
@@ -76,32 +84,33 @@ useEffect(() => {
       <img className={stylesEmailLogin.koalingoLogo} alt="" src="../koalingo_logo.svg" />
 
       <div className={stylesHome.mainWrapper}>
-          <RecWrapper
-            content={
-              <div className={stylesHome.gamepinWrapper}>
-                <input className={stylesHome.enter} type="text"
-                        nKeyPress={(event) => {
-                          if (!/[0-9]/.test(event.key)) {
-                          event.preventDefault(); // Only allow numbers https://stackoverflow.com/questions/43687964/only-numbers-input-number-in-react
-                          }
-                        }} 
-                        placeholder="Game PIN here!"
-                        value={gamePinEntered}
-                        onChange={handleTextChange}
-                />
-                <Button btnText='Submit' onClick={checkGamePinOnSubmit}
-                        btnStyle='purple'/>
-              </div>
-            }
-          />
+        {/* See RecWrapper declaration for docs*/}
+        <RecWrapper
+          content={
+            <div className={stylesHome.gamepinWrapper}> {/* Provide styles for gamepin section*/}
+              <input className={stylesHome.enter} type="text"
+                nKeyPress={(event) => {
+                  if (!/[0-9]/.test(event.key)) {
+                    event.preventDefault(); // Only allow numbers https://stackoverflow.com/questions/43687964/only-numbers-input-number-in-react
+                  }
+                }}
+                placeholder="Game PIN here!"
+                value={gamePinEntered}
+                onChange={handleTextChange}
+              />
+              <Button btnText='Submit' onClick={checkGamePinOnSubmit}
+                btnStyle='purple' />
+            </div>
+          }
+        />
       </div>
 
-      <div className={stylesHome.bottomWrapper}>
+      <div className={stylesHome.bottomWrapper}>  {/* Provide styles for bottom section*/}
         <Button btnText="Create Game" onClick={onCreateAGameClick}
-                btnStyle="gold" length="btnLong"/>
+          btnStyle="gold" length="btnLong" />
         <Button btnText="Sign out" onClick={logout}
-                 btnStyle="bgColor" />
-        </div>
+          btnStyle="bgColor" />
+      </div>
     </div>
   )
 };
--- koalingo/src/pages/home/PlayerCustomise.js
+++ koalingo/src/pages/home/PlayerCustomise.js
@@ -5,43 +5,53 @@ import stylesLogin from "./login.module.css";
 import stylesCustomise from "./playerCustomise.module.css";
 import stylesEmailLogin from "./emailLogin.module.css";
 import { realtimedb } from "../../firebase";
-import { ref, update} from "firebase/database";
+import { ref, update } from "firebase/database";
 import React, { useState } from "react";
 import { getAuth } from "firebase/auth";
 import { AuthContext } from "../../App";
 import RecWrapper from "../../components/rectangleWrapper/Wrapper";
 import "../../App"
 
+/**
+ * Player Customisation page
+ * @returns {JSX.Element}
+ */
 export const PlayerCustomise = () => {
   const navigate = useNavigate();
   const auth = getAuth();
   const user = auth.currentUser;
-  const {setCharacter, setGlobNickname} = useContext(AuthContext);
+  const { setCharacter, setGlobNickname } = useContext(AuthContext);
   const [nickname, setNickname] = useState("");
 
   // Directly pulled from storage folder on firebase
   const girl_url = 'https://firebasestorage.googleapis.com/v0/b/koalingo-dc436.appspot.com/o/initial-wave-girl.gif?alt=media&token=f9a02670-c2fa-423b-877f-429c81f29f9d';
   const boy_url = 'https://firebasestorage.googleapis.com/v0/b/koalingo-dc436.appspot.com/o/initial-wave-boy.gif?alt=media&token=f87acbde-d8c5-4bad-8b8c-06564f39a0dc';
 
-  const {gamePin} = useContext(AuthContext)
+  const { gamePin } = useContext(AuthContext)
 
+  /**
+   * Adds nickname to firebase
+   * @returns {Promise<void>}
+   */
   const addNickname = async () => {
     const reference = ref(realtimedb, "games/" + gamePin + "/players/" + user.uid);
     // Create a new game
-    await update(reference, {name : nickname});
+    await update(reference, { name: nickname });
   };
 
+  // Define event listeners
   const onChangeNickname = (event) => {
     setNickname(event.target.value);
   }
 
+  // Set character and nickname to context provider when clicked
   const onClickChar = (event) => {
     const char = event.currentTarget.id;
     setCharacter(char);
     setGlobNickname(nickname);
 
     if (nickname != "") {
-      addNickname(event.currentTarget.id)
+      addNickname(event.currentTarget.id)  // Add nickname to firebase
       navigate('/waiting');
     } else {
       alert('Sorry, you must choose a nickname!')
@@ -51,31 +61,38 @@ export const PlayerCustomise = () => {
   return (
     <div className={stylesLogin.loginPage}>
       <img className={stylesEmailLogin.koalingoLogo} alt="" src="../koalingo_logo.svg" />
-  
+
       <div className={stylesHome.mainWrapper}>
-          <RecWrapper size="heightFit"
-                      content={
-                        <div className={stylesCustomise.mainWrapperChild}>
-                          Your Nickame
-                          <input className={stylesCustomise.inputWrapper}
-                            placeholder='Enter Nickname'
-                            value={nickname}
-                            onChange={onChangeNickname}
-                          />
-                          <div className={stylesCustomise.characterWrapper}>
-                          Choose your character!
-                            <div className={stylesCustomise.characterWrapperChild}>
-                              <button id='girl' className={stylesCustomise.child} onClick={onClickChar}>
-                                <img src={girl_url} alt="girl failed to load"/>
-                              </button>
-                              <button id='boy' className={stylesCustomise.child} onClick={onClickChar}>
-                                <img src={boy_url} alt="boy failed to load"/>
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      }
-          />
+        {/*Wrap whole component in white box */}
+        <RecWrapper size="heightFit"
+          content={
+            // Input box for nickname
+            <div className={stylesCustomise.mainWrapperChild}>
+              Your Nickame
+
+              <input className={stylesCustomise.inputWrapper}
+                placeholder='Enter Nickname'
+                value={nickname}
+                onChange={onChangeNickname}
+              />
+
+              {/*Character selection section*/}
+              <div className={stylesCustomise.characterWrapper}>
+                Choose your character!
+                <div className={stylesCustomise.characterWrapperChild}>
+
+                  <button id='girl' className={stylesCustomise.child} onClick={onClickChar}>
+                    <img src={girl_url} alt="girl failed to load" />
+                  </button>
+                  <button id='boy' className={stylesCustomise.child} onClick={onClickChar}>
+                    <img src={boy_url} alt="boy failed to load" />
+                  </button>
+                  
+                </div>
+              </div>
+            </div>
+          }
+        />
       </div>
     </div>
   )
