78ce1d57d8a0b3dc28d9275a62f5eaca61c17df5
gurooria <gloriasun528@gmail.com>
2023-01-16 08:37:43 +0000

    dev buttons & messages

--- koalingo/src/pages/player/Answer_wrong.js
+++ koalingo/src/pages/player/Answer_wrong.js
@@ -3,9 +3,13 @@ import stylesResult from './state-result.module.css';
 import stylesEmailLogin from '../home/emailLogin.module.css';
 import RecWrapper from '../../components/rectangleWrapper/Wrapper';
 import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 const StateResult = () => {
   const navigate = useNavigate();
+  const onNextClick = useCallback(() => {
+    navigate("/end");
+  }, [navigate]);
   
 return (
   <div className={stylesResult.backgroundFalse}>
@@ -15,11 +19,14 @@ return (
       <RecWrapper length="heightFit"
                   content={
                     <div className={stylesResult.recWrapper}>
-                      <div className={stylesResult.textWrapper1}>Unfortunately, this is uncorrect, you will do better next time! </div>
+                      <div className={stylesResult.textWrapper1}>Unfortunately, this is incorrect, you will do better next time! </div>
                       <div className={stylesResult.textWrapper2}>+ 20 XP for the team</div>
                     </div>
                   }
       />
+    </div>
+    <div className={stylesResult.devButton}>
+      <button onClick={onNextClick}> dev button: if stuck, press to proceed</button>
     </div>
   </div>
 );
